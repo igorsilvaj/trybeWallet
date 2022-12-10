ad20e584cccc71ee1880328a4cd70abdf4501c87
IgorSilvaJ <igorjsilvabiz@gmail.com>
2022-12-10 01:41:01 -0300

    Adiciona requisito 6

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -5,7 +5,22 @@ class Table extends Component {
   render() {
     return (
       <div className="expenseTableContainer">
-        <table className="expenseTable" />
+        <table className="expenseTable">
+          <thead>
+            <tr>
+              <th>Descrição</th>
+              <th>Tag</th>
+              <th>Método de pagamento</th>
+              <th>Valor</th>
+              <th>Moeda</th>
+              <th>Câmbio utilizado</th>
+              <th>Valor convertido</th>
+              <th>Moeda de conversão</th>
+              <th>Editar/Excluir</th>
+            </tr>
+          </thead>
+          <tbody />
+        </table>
       </div>
     );
   }
